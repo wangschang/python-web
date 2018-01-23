83dee171f7a534e8b1f866507dda4ed4e441ab0b
wangschang <wangschang@126.com>
2018-01-23 16:14:33 +0800

    post.py
    发送post请求

---
+++ post.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+调用post
+'''
+import os,sys
+import urllib,urllib2
+
+def main():
+    url = 'http://xxxx/msg/HttpBatchSendSM'
+    msg = '您的验证码是123456'
+    values = {  
+            'msg' : msg,
+            'mobile':'18518100104'
+             }   
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+    print('%s' % the_page)
+
+if __name__ == '__main__':
+    main()
